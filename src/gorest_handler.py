f102904fba140c9310e79b96110ccbae85d661cf
paciadawid <paciadawid@gmail.com>
2022-01-04 16:54:48 +0100

    gorest test

--- src/gorest_handler.py
+++ src/gorest_handler.py
@@ -23,3 +23,11 @@ class GorestAPIHandler:
     def get_user(self, user_id):
         res = requests.get(f"{self.base_url}{self.user_url}/{user_id}", headers=self.token)
         return res.json(), res.status_code
+
+    def update_user(self, user_id, new_data):
+        res = requests.put(f"{self.base_url}{self.user_url}/{user_id}", data=new_data, headers=self.token)
+        return res.json(), res.status_code
+
+    def delete_user(self, user_id):
+        res = requests.delete(f"{self.base_url}{self.user_url}/{user_id}", headers=self.token)
+        return res.status_code
--- tests/test_gorest.py
+++ tests/test_gorest.py
@@ -1,16 +1,28 @@
 import unittest
-import time
-from faker import Faker
-import requests
+
 from src.gorest_handler import GorestAPIHandler
+
 
 class TestGorest(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.gorest_handler = GorestAPIHandler("Bearer 106f3e7a3a32bcf14303de2cbfa28452a0e1dd3880d2a11676bc11971f186ebb")
+        self.gorest_handler = GorestAPIHandler(
+            "Bearer 106f3e7a3a32bcf14303de2cbfa28452a0e1dd3880d2a11676bc11971f186ebb")
 
     def test_user_create(self):
-        response_body, status_code = self.gorest_handler.create_user()
+        response_body, _ = self.gorest_handler.create_user()
         self.assertIn("id", response_body["data"])
         response_body, status_code = self.gorest_handler.get_user(response_body["data"]["id"])
         self.assertEqual(status_code, 200)
+
+    def test_user_update(self):
+        new_name = {"name": "Test Test"}
+        response_body, _ = self.gorest_handler.create_user()
+        response_body, status_code = self.gorest_handler.update_user(response_body["data"]["id"], new_name)
+        self.assertEqual(status_code, 200)
+        self.assertEqual(response_body["data"]["name"], new_name["name"])
+
+    def test_user_delete(self):
+        response_body, _ = self.gorest_handler.create_user()
+        status_code = self.gorest_handler.delete_user(response_body["data"]["id"])
+        self.assertEqual(status_code, 204)
