5c5d9c9313652a98e5301d4e6234adac047dbdfc
paciadawid <paciadawid@gmail.com>
2022-01-04 16:09:39 +0100

    gorest test

---
+++ src/gorest_handler.py
@@ -0,0 +1,25 @@
+import requests
+from faker import Faker
+
+
+class GorestAPIHandler:
+    base_url = "https://gorest.co.in/public/v1"
+    user_url = "/users"
+
+    def __init__(self, token):
+        self.token = {"Authorization": token}
+        self.fake = Faker()
+
+    def create_user(self):
+        user = {
+            "name": self.fake.name(),
+            "gender": "male",
+            "email": self.fake.email(),
+            "status": "active"
+        }
+        res = requests.post(f"{self.base_url}{self.user_url}", data=user, headers=self.token)
+        return res.json(), res.status_code
+
+    def get_user(self, user_id):
+        res = requests.get(f"{self.base_url}{self.user_url}/{user_id}", headers=self.token)
+        return res.json(), res.status_code
--- tests/test_gorest.py
+++ tests/test_gorest.py
@@ -2,30 +2,15 @@ import unittest
 import time
 from faker import Faker
 import requests
-
+from src.gorest_handler import GorestAPIHandler
 
 class TestGorest(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.token = {
-            "Authorization": "Bearer 106f3e7a3a32bcf14303de2cbfa28452a0e1dd3880d2a11676bc11971f186ebb"
-        }
-        self.fake = Faker()
-        self.base_url = "https://gorest.co.in/public/v1"
-        self.user_url = "/users"
+        self.gorest_handler = GorestAPIHandler("Bearer 106f3e7a3a32bcf14303de2cbfa28452a0e1dd3880d2a11676bc11971f186ebb")
 
     def test_user_create(self):
-        user = {
-            "name": self.fake.name(),
-            "gender": "male",
-            "email": self.fake.email(),
-            "status": "active"
-        }
-
-        response_body = requests.post(f"{self.base_url}{self.user_url}", data=user, headers=self.token).json()
+        response_body, status_code = self.gorest_handler.create_user()
         self.assertIn("id", response_body["data"])
-        id = response_body["data"]["id"]
-        res2 = requests.get(f"{self.base_url}{self.user_url}/{id}", headers=self.token)
-
-        self.assertEqual(res2.status_code, 200)
-        self.assertDictContainsSubset(user, res2.json()["data"])
+        response_body, status_code = self.gorest_handler.get_user(response_body["data"]["id"])
+        self.assertEqual(status_code, 200)
